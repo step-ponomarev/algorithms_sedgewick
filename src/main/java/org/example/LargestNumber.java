607a3ad84c48f97a40fa925fbdbe3e6b94a076ec
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2024-06-21 00:38:11 +0300

    works

---
+++ src/main/java/org/example/LargestNumber.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+
+// https://stepik.org/lesson/864352/step/1?unit=868436
+public final class LargestNumber {
+    public static void main(String[] args) {
+        System.out.println(largestNumber(new String[]{"4444", "46"}));
+    }
+
+    private static String largestNumber(String[] a) {
+        Arrays.sort(a, (l, r) -> (Integer.parseInt(r + l) - Integer.parseInt(l + r)));
+
+        return String.join("", a);
+    }
+}
